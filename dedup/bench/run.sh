749c20d6512516982642cda2a3d4458eb46921e4
Dimitrios Chasapis <dimitrios.chasapis@bsc.es>
2016-04-27 18:57:48 +0200

    added install_inputs.sh script

--- dedup/bench/run.sh
+++ dedup/bench/run.sh
@@ -10,9 +10,9 @@ BENCHPATH=${ROOT}/${BENCHMARK}
 
 case $INPUT in
 	"native") ARGS="-c -p -v -i ${BENCHPATH}/inputs/FC-6-x86_64-disc1.iso -o ${BENCHPATH}/outputs/output.dat.ddp";;
-	"simlarge") ARGS="-c -p -v -i ${BENCHPATH}/inputs/media_l.dat -o ${BENCHPATH}/outputs/output.dat.ddp";;
-	"simmedium") ARGS="-c -p -v -i ${BENCHPATH}/inputs/media_m.dat -o ${BENCHPATH}/outputs/output.dat.ddp";;
-	"simsmall") ARGS="-c -p -v -i ${BENCHPATH}/inputs/media_s.dat -o ${BENCHPATH}/outputs/output.dat.ddp";;
+	"simlarge") ARGS="-c -p -v -i ${BENCHPATH}/inputs/media.dat -o ${BENCHPATH}/outputs/output.dat.ddp";;
+	"simmedium") ARGS="-c -p -v -i ${BENCHPATH}/inputs/media.dat -o ${BENCHPATH}/outputs/output.dat.ddp";;
+	"simsmall") ARGS="-c -p -v -i ${BENCHPATH}/inputs/media.dat -o ${BENCHPATH}/outputs/output.dat.ddp";;
 	"simdev") ARGS="-c -p -v -i ${BENCHPATH}/inputs/hamlet.dat -o ${BENCHPATH}/outputs/output.dat.ddp";;
 	"test") ARGS="-c -p -v -i ${BENCHPATH}/inputs/test.dat -o ${BENCHPATH}/outputs/output.dat.ddp";;
 esac
